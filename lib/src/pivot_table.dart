878c3ea02ac5b33c32c59717f628b0911d9cfb1a
George Walker <george.s.walker.1994@gmail.com>
2024-02-27 15:08:46 +0000

    Abandon Pivot table for now

--- lib/src/pivot_table.dart
+++ lib/src/pivot_table.dart
@@ -1,3 +1,4 @@
+/*
 // ignore_for_file: no_leading_underscores_for_local_identifiers
 
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ class PivotTable extends StatefulWidget {
     required this.headers,
     required this.data,
     this.columnWidth = 100.0,
+    this.onRowSummarization,
   });
 
   /// the background color
@@ -16,21 +18,105 @@ class PivotTable extends StatefulWidget {
 
   final List<String> headers;
 
-  final List<Map<String, dynamic>> data;
+  final List<Map<String, List<int>>> data;
 
   final double columnWidth;
+
+  final String Function(dynamic)? onRowSummarization;
 
   @override
   State<PivotTable> createState() => _PivotTableState();
 }
 
 class _PivotTableState extends State<PivotTable> {
-  bool isExpanded = false;
-  bool baselineExpanded = false;
-  bool ambitionExpanded = false;
-  bool counterFactualExpanded = false;
-  bool factualExpanded = false;
-  bool actualExpanded = false;
+  Map<int, bool> isRowExpanded = {};
+
+  List<TableRow> _buildRows() {
+    List<TableRow> _rows = [];
+
+    // Initialize the isRowExpanded map
+    for(int i = 0; i < widget.data.length; i++){
+      if(!isRowExpanded.keys.contains(i)){
+        isRowExpanded[i] = false;
+      }
+    }
+
+    for (Map<String, List<int>> data in widget.data) {
+      List<TableRow> tableRows = [];
+      for (String key in data.keys) {
+        tableRows.addAll(convertToTableRows(data[key]!, isRowExpanded));
+      }
+      _rows.addAll(tableRows);
+    }
+
+    return _rows;
+  }
+
+  List<TableRow> convertToTableRows(List<int> data, Map<int, bool> isRowExpanded, String key) {
+    List<TableRow> tableRows = [];
+    int sum = 0;
+    bool isTotalRow = false;
+
+    for (int i = 0; i < data.length; i++) {
+      if (isRowExpanded.containsKey(i)) {
+        isTotalRow = isRowExpanded[i]!;
+        if (isTotalRow) {
+          tableRows.add(_createTotalRow(sum, key));
+          sum = 0;
+        }
+      }
+
+      String name = "";
+      int? value;
+      if (i < data.length) {
+        name = data[i].toString(); // Assuming data[i] is a name represented as an integer
+        value = data[i];
+      }
+
+      if (!isTotalRow) {
+        sum += value!;
+        tableRows.add(_createIndividualRow(name, value));
+      }
+    }
+
+    if (!isTotalRow && sum > 0) {
+      tableRows.add(_createTotalRow(sum, key));
+    }
+
+    return tableRows;
+  }
+
+  TableRow _createTotalRow(int total, String key) {
+    return TableRow(
+      children: [
+        const TableCell(
+          child: Text(
+            "Total",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        TableCell(
+          child: Text(key, style: const TextStyle(fontWeight: FontWeight.bold)),
+        ),
+        TableCell(
+          child: Text(total.toString(), style: const TextStyle(fontWeight: FontWeight.bold)),
+        ),
+      ],
+    );
+  }
+
+  TableRow _createIndividualRow(String name, int? value) {
+    return TableRow(
+      children: [
+        TableCell(
+          child: Text(name),
+        ),
+        TableCell(
+          child: Text(value != null ? value.toString() : ""),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,6 +162,8 @@ class _PivotTableState extends State<PivotTable> {
                                 )),
                               ],
                             ),
+                            // Body
+                            ..._buildRows(),
                           ],
                         ),
                       ],
@@ -87,3 +175,4 @@ class _PivotTableState extends State<PivotTable> {
 
   }
 }
+*/
